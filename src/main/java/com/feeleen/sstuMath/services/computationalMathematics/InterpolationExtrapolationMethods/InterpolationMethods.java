c0e99a056f69c3a46c7837b89aaee598be1820df
Pavel <pavel164rus1@gmail.com>
2019-03-23 22:04:29 +0400

    pre-refactoring gaussMethod

--- src/main/java/com/feeleen/sstuMath/services/computationalMathematics/InterpolationExtrapolationMethods/InterpolationMethods.java
+++ src/main/java/com/feeleen/sstuMath/services/computationalMathematics/InterpolationExtrapolationMethods/InterpolationMethods.java
@@ -3,23 +3,34 @@ package com.feeleen.sstuMath.services.computationalMathematics.InterpolationExtr
 import java.util.List;
 
 public class InterpolationMethods {
-    public static double gausMethod(double x, double[][] xy) {
+    public static double gaussMethod(double x, double[][] xy) {
 
         DiagonalFiniteDifferenceTable table = new DiagonalFiniteDifferenceTable(xy);
 
         List<List<Double>> tableEntry = table.getTableEntry();
         double t = (x - table.getLeftX(x)) / table.getH();
+
+        System.out.println("t = " + t);
         int index = table.getIndexLeftX(x);
 
-        //todo index from length
-        return table.getYList().get(index)
-                + (getColumn(index, 0, table.getDiffList(0))) * t
-                + getColumn(index, 1, table.getDiffList(1)) * t * (t - 1) / 6
-                + ((t - 1) * t * (t + 1)) * getColumn(index, 2, table.getDiffList(2)) / factorial(3);
+        double a0 = table.getYList().get(index);
+        double a1 = (getColumn(index, 0, table.getDiffList(0))) * t;
+        double a2 = getColumn(index, 1, table.getDiffList(1)) * t * (t - 1) / 6;
+        double a3 = ((t - 1) * t * (t + 1)) * getColumn(index, 2, table.getDiffList(2)) / factorial(3);
+        System.out.println("a0 = " + a0);
+        System.out.println("a1 = " + a1);
+        System.out.println("a2 = " + a2);
+        System.out.println("a3 = " + a3);
+        double result = a0
+                + a1
+                + a2
+                + a3;
+        return result;
     }
 
     private static double getColumn(int index, int diffNumber, List<Double> list) {
         Double aDouble = list.get(index - (diffNumber));
+        System.out.println("l: " +  aDouble);
         return aDouble;
     }
 
@@ -35,9 +46,13 @@ public class InterpolationMethods {
         int index = table.getIndexLeftX(x);
 
         //todo index from length and add check for intdex of table.getDiffList()
-        return (table.getYList().get(index) + table.getYList().get(index - 1)) / 2
-                + (t * (t - 1) / factorial(2)) * getColumn(index, 0, table.getDiffList(0)) * table.getDiffList(0).get(0) / 2
-                + (t - 1 / 2) * t * (t - 1) / factorial(3) * table.getDiffList(2).get(-1);
+        double a0 = (table.getYList().get(index) + table.getYList().get(index - 1)) / 2;
+        double a1 = (t * (t - 1) / factorial(2)) * getColumn(index, 0, table.getDiffList(0)) * table.getDiffList(0).get(0) / 2;
+        double a2 = (t - 1 / 2) * t * (t - 1) / factorial(3) * table.getDiffList(2).get(-1);
+        double result = a0
+                + a1
+                + a2;
+        return result;
 
     }
 
--- src/test/java/com/feeleen/sstuMath/services/computationalMathematics/InterpolationExtrapolationMethods/DiagonalFiniteDifferenceTableTest.java
+++ src/test/java/com/feeleen/sstuMath/services/computationalMathematics/InterpolationExtrapolationMethods/DiagonalFiniteDifferenceTableTest.java
@@ -1,6 +1,5 @@
 package com.feeleen.sstuMath.services.computationalMathematics.InterpolationExtrapolationMethods;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,44 +18,57 @@ public class DiagonalFiniteDifferenceTableTest {
     @Before
     public void setData() {
         data = new double[][]{
-                {1.50, 15.132},
-                {1.55, 17.422},
-                {1.60, 20.393},
-                {1.65, 23.994},
-                {1.70, 28.160},
-                {1.75, 32.812},
-                {1.80, 37.857},
-                {1.85, 43.189},
-                {1.90, 46.689},
-                {1.95, 54.225},
-                {2.00, 59.653},
-                {2.05, 64.817},
-                {2.10, 69.550},
+                {0.12, 6.278},
+                {0.14, 6.405},
+                {0.16, 6.487},
+                {0.18, 6.505},
+                {0.20, 6.436},
+                {0.22, 6.259},
+                {0.24, 5.954},
         };
     }
 
-
-//    public void setLists() {
-//        ArrayList<Double> row1 = new ArrayList<>();
-//        ArrayList<Double> row2 = new ArrayList<>();
-//        listsExpected.add(row1);
-//        listsExpected.add(row2);
-//        row1.add(0.12);
-//        row1.add(0.14);
-//        row1.add(0.16);
-//        row1.add(0.18);
-//        row1.add(0.20);
-//        row1.add(0.22);
-//        row1.add(0.24);
-//
-//        row2.add(6.278);
-//        row2.add(6.405);
-//        row2.add(6.487);
-//        row2.add(6.505);
-//        row2.add(6.436);
-//        row2.add(6.259);
-//        row2.add(5.954);
+//    @Before
+//    public void setData() {
+//        data = new double[][]{
+//                {1.50, 15.132},
+//                {1.55, 17.422},
+//                {1.60, 20.393},
+//                {1.65, 23.994},
+//                {1.70, 28.160},
+//                {1.75, 32.812},
+//                {1.80, 37.857},
+//                {1.85, 43.189},
+//                {1.90, 46.689},
+//                {1.95, 54.225},
+//                {2.00, 59.653},
+//                {2.05, 64.817},
+//                {2.10, 69.550},
+//        };
 //    }
+
+
+    public void setLists() {
+        ArrayList<Double> row1 = new ArrayList<>();
+        ArrayList<Double> row2 = new ArrayList<>();
+        listsExpected.add(row1);
+        listsExpected.add(row2);
+        row1.add(0.12);
+        row1.add(0.14);
+        row1.add(0.16);
+        row1.add(0.18);
+        row1.add(0.20);
+        row1.add(0.22);
+        row1.add(0.24);
+
+        row2.add(6.278);
+        row2.add(6.405);
+        row2.add(6.487);
+        row2.add(6.505);
+        row2.add(6.436);
+        row2.add(6.259);
+        row2.add(5.954);
+    }
 
     @Before
     public void setList2() {
@@ -95,7 +107,6 @@ public class DiagonalFiniteDifferenceTableTest {
 
     @Before
     public void setTableMock() {
-
         ArrayList<Double> row1 = new ArrayList<>();
         ArrayList<Double> row2 = new ArrayList<>();
         ArrayList<Double> row3 = new ArrayList<>();
@@ -149,12 +160,23 @@ public class DiagonalFiniteDifferenceTableTest {
         for (int i = 0; i <= 30; i++) {
 
             double x = getX(i);
-            double v = InterpolationMethods.gausMethod(x, data);
-            System.out.println( x + "  :  " + v);
+            double v = InterpolationMethods.gaussMethod(x, data);
+            System.out.println(x + "  :  " + v);
         }
 
         assertThat(tableExpected, is(table.getTableEntry()));
     }
+
+    @Test
+    public void getTable2() {
+        DiagonalFiniteDifferenceTable table = new DiagonalFiniteDifferenceTable(data);
+
+        double v = InterpolationMethods.gaussMethod(0.168, data);
+
+
+        assertThat(tableExpected, is(table.getTableEntry()));
+    }
+
 
     public double getX(int n) {
         return 1.725 + 0.002 * n;
